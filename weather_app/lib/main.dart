fe9036e9c50a107d07ee7c4012648a2fb1ac9957
MierafA12 <mierafabebe12@gmail.com>
2025-07-25 15:06:06 +0300

    beginig

---
+++ weather_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner:false 
+    );
+  }
+
+}
