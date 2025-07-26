88181817ee4c8c18761db1b93279ddc9b9de98ab
MierafA12 <mierafabebe12@gmail.com>
2025-07-26 14:56:24 +0300

    work text filedparts

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main(){
-  runApp(WeatherApp());
+void main() {
+  runApp(const WeatherApp());
 }
 
-class WeatherApp extends StatelessWidget{
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    debugShowCheckedModeBanner:false 
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WeatherHome(),
     );
   }
+}
 
+class WeatherHome extends StatefulWidget {
+  const WeatherHome({super.key});
+
+  @override
+  State<WeatherHome> createState() => _WeatherHomeState();
+}
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Weather App')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                labelText: 'Enter Country or City',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                final input = _controller.text.trim();
+                if (input.isNotEmpty) {
+                  fetchWeather(input);
+                } else {
+                  showError("Please enter a country or city name.");
+                }
+              },
+              child: const Text('Get Weather'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
