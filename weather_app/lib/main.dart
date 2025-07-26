e82a351abf8e55f9dea98fdf0360a09fb56cf433
MierafA12 <mierafabebe12@gmail.com>
2025-07-26 15:11:08 +0300

    adding api to fetch weather

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -25,7 +25,60 @@ class WeatherHome extends StatefulWidget {
   State<WeatherHome> createState() => _WeatherHomeState();
 }
 
+class _WeatherHomeState extends State<WeatherHome> {
+  final TextEditingController _controller = TextEditingController();
+  final String apiKey = ''; //  Replace by OpenWeatherMap API Key
 
+  Future<void> fetchWeather(String country) async {
+    final url =
+        'https://api.openweathermap.org/data/2.5/weather?q=$country&appid=$apiKey&units=metric';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+
+        final temp = data['main']['temp'];
+        final desc = data['weather'][0]['description'];
+        final city = data['name'];
+
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: Text('Weather in $city'),
+            content: Text('Temperature: $temp°C\nDescription: $desc'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("OK"),
+              )
+            ],
+          ),
+        );
+      } else {
+        showError("Country not found or error fetching data.");
+      }
+    } catch (e) {
+      showError("Something went wrong. Please try again.");
+    }
+  }
+
+  void showError(String message) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text("Error"),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Close"),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,3 +114,5 @@ class WeatherHome extends StatefulWidget {
     );
   }
 }
+
+
